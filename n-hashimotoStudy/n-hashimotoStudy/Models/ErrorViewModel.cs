0cca6f68b8644674500527be1a85eb0546aa732e
n-hashimoto2193 <n-hashimoto@chancelab.jp>
2019-11-11 14:59:47 +0900

    開発直後

---
+++ n-hashimotoStudy/n-hashimotoStudy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace n_hashimotoStudy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
