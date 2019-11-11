a68a811a14fd228fff8a08f23a8a1a9aad7d141b
n-hashimoto2193 <n-hashimoto@chancelab.jp>
2019-11-11 16:18:34 +0900

    マスターメンテ　TOP

---
+++ n-hashimotoStudy/n-hashimotoStudy/Controllers/MasterMainteController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace n_hashimotoStudy.Controllers
+{
+    public class MasterMainteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
